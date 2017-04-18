f2c4a33fea19a5089c63c483d468e9a253c76fac
DHOMD <DHO@dho.im>
2017-04-18 16:31:12 -0400

    Revert "Broken"
    This reverts commit 5c01f801e2651474067d4afec6feaf8eec6c2a27.

--- cm.mk
+++ cm.mk
@@ -1,22 +1,22 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/broken/config/common.mk)
+$(call inherit-product, vendor/cm/config/cdma.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/ls995/ls995.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls995
-PRODUCT_NAME := broken_ls995
+PRODUCT_NAME := cm_ls995
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-LS995
 PRODUCT_MANUFACTURER := LGE
@@ -25,4 +25,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=lge/z_tmo_us/zee:4.4.2/KOT49I.D95920h/D95920h.1398665038:user/release-keys \
     PRIVATE_BUILD_DESC="z_tmo_us-user 4.4.2 KOT49I.D95920h D95920h.1398665038 release-keys"
 
-
+PRODUCT_PACKAGES += Torch
